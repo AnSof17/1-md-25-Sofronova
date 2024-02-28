8061000d0101dd4af5036ac1175721b7f1903c1b
AnSof17 <sofronka10@gmail.com>
2024-02-28 12:13:09 +0300

    Задание 1

---
+++ ПР1№1.py
@@ -0,0 +1,9 @@
+"""Задание 1"""
+print("Enter the password: ")
+a = int(input())
+print("Confirm the password: ")
+b = int(input())
+if a ==b:
+    print("The password is accepted. ")
+else:
+    print("The password is not accepted.")
