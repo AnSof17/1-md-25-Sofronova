a6250e71191d36c212a74b4f306ce27f595652e4
AnSof17 <159244944+AnSof17@users.noreply.github.com>
2024-02-28 12:15:52 +0300

    Delete ПР1.py

--- ПР1.py
+++
@@ -1,9 +0,0 @@
-"""Задание 1"""
-print("Enter the password: ")
-a = int(input())
-print("Confirm the password: ")
-b = int(input())
-if a ==b:
-    print("The password is accepted. ")
-else:
-    print("The password is not accepted.")
